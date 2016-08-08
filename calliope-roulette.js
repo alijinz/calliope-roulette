34d716a81cb6ff115e50df9fac3cc61a4eee1728
orzoxcv <orzoxcv@gmail.com>
2016-08-08 13:15:39 -0400

    Update calliope-roulette.js

--- calliope-roulette.js
+++ calliope-roulette.js
@@ -2,6 +2,5 @@ var items = ["dog", "cat", "horse", "cow"];
 
 function myFunction() {
     var x = document.getElementById("testing");
-    var y = Math.floor((Math.random() * 4) + 0);
-    x.innerHTML = items[y];
+    x.innerHTML = Math.floor((Math.random() * 4) + 0);
 }
