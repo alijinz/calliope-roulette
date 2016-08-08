3611bcd973ba67b998a8e0dcb8fd09be41a912fd
orzoxcv <orzoxcv@gmail.com>
2016-08-08 15:45:06 -0400

    Update calliope-roulette.js

--- calliope-roulette.js
+++ calliope-roulette.js
@@ -1,11 +1,82 @@
+
 var items = [];
+var charries = [];
+var char1;
+var char2;
 
 $.get('https://rawgit.com/orzoxcv/calliope-roulette/master/items.txt', function(data) {
     items = data.split("\n");
 });
 
-function myFunction() {
-    var x = document.getElementById("testing");
-    var y = Math.floor((Math.random() * items.length) + 0);
-    x.innerHTML = items[y] + " " + items.length;
+// $.get('https://rawgit.com/orzoxcv/calliope-roulette/master/characters.txt', function(data) {
+//     charries = data.split("\n");
+// });
+
+function randomValueFromArray(array){
+  var random = Math.floor(Math.random()*(array.length-1))
+  return array[random];
+}
+
+function initializeCharries(){
+	var x = document.getElementById("myText").value;
+	charries = x.split("\n");
+	char1 = Math.floor(Math.random() * (charries.length-1));
+	char2 = char1;
+	while(char2 == char1){
+		char2 = Math.floor(Math.random() * (charries.length-1));
+	}
+}
+
+function myFunction(){
+	var text = document.getElementById("testing");
+	initializeCharries();
+
+	//***GENERATE A SCENARIO***//
+
+	var x = Math.floor((Math.random() * 5) + 1);
+
+	var scenario = "For some reason, ";
+
+	if (x==1){
+		scenario += ":P1: gives :P2: :a(n): :xitem:.";
+	}
+	else if (x==2){
+		scenario += ":P1: sees :P2: walking around with :a(n): :xitem:."
+	}
+	else if (x==3){
+		scenario += ":P1: steals :a(n): :xitem: from :P2:."
+	}
+	else if (x==4){
+		scenario += ":P1: needs :P2:'s :xitem:."
+	}
+	else {
+		scenario += ":P1: has :P2:'s :xitem:."
+	}
+
+
+	//***MAD LIBS REPLACEAROO***//
+	var item = randomValueFromArray(items);
+
+	scenario = scenario.replace(":P1:", charries[char1]);
+	scenario = scenario.replace(":P2:", charries[char2]);
+	scenario = scenario.replace(":xitem:", item);
+
+
+	//***GRAMMAR STUFF***//
+	var firstletteritem = item.substring(0,1);
+
+	if (firstletteritem.localeCompare("a") == 0 || 
+		firstletteritem.localeCompare("e") == 0 || 
+		firstletteritem.localeCompare("i") == 0 ||
+		firstletteritem.localeCompare("o") == 0 ||
+		firstletteritem.localeCompare("u") == 0) {
+
+		scenario = scenario.replace(":a(n):", "an");
+
+	} else {
+		scenario = scenario.replace(":a(n):", "a");
+	}
+
+	//***OUTPUT***//
+	text.innerHTML = scenario;
 }
