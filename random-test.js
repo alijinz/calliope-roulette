da2e6214f3a6d942220c45397eb613960df247c6
orzoxcv <orzoxcv@gmail.com>
2016-08-08 16:05:05 -0400

    Update random-test.js

--- random-test.js
+++ random-test.js
@@ -1,11 +1,11 @@
-var items = [];
+var animals = [];
 
 $.get('https://rawgit.com/orzoxcv/calliope-roulette/master/animals.txt', function(data) {
-    items = data.split("\n");
+    animals = data.split("\n");
 });
 
 function myFunction() {
     var x = document.getElementById("testing");
-    var y = Math.floor((Math.random() * items.length) + 0);
-    x.innerHTML = items[y];
+    var y = Math.floor((Math.random() * animals.length) + 0);
+    x.innerHTML = animals[y];
 }
